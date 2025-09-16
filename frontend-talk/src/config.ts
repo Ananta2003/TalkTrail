24a5e85244a7c5a575aa10f73cc276d2f718bbf3
Ananta <anantavigave@gmail.com>
2025-09-16 20:47:26 +0530

    added

--- frontend-talk/src/config.ts
+++ frontend-talk/src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = ("https://talktrail-6p1m.onrender.com/")
+export const BACKEND_URL = ("https://talktrail-6p1m.onrender.com")
 
 export const scrollToElement = (id: string) => {
   const element = document.getElementById(id);
